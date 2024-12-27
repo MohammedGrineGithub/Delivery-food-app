05ae560dd3f3ec539cf72b9d0f0871dfa07cfea4
Loordm <lm_grine@esi.dz>
2024-12-27 20:40:18 +0100

    Add font family (Ibmplexsans and Lemon)

--- app/src/main/java/com/example/deliveryfoodapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/deliveryfoodapp/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.deliveryfoodapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.deliveryfoodapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -30,4 +32,17 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val ibmplexsansFontFamily = FontFamily(
+    Font(R.font.ibmplexsans_bold, FontWeight.Bold),
+    Font(R.font.ibmplexsans_extralight, FontWeight.ExtraLight),
+    Font(R.font.ibmplexsans_light, FontWeight.Light),
+    Font(R.font.ibmplexsans_medium, FontWeight.Medium),
+    Font(R.font.ibmplexsans_regular, FontWeight.Normal),
+    Font(R.font.ibmplexsans_semibold, FontWeight.SemiBold)
+)
+
+val lemonFontFamily = FontFamily(
+    Font(R.font.lemon_regular)
 )
