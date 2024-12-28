bd2aac01fd71b7403360891ca8f19035210a9fc6
Loordm <lm_grine@esi.dz>
2024-12-28 01:27:03 +0100

    Add the permission to let the app in portrait mode only

--- app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -57,4 +58,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainApp(navController : NavHostController, modifier: Modifier = Modifier) {
+    Text(text = "Main App")
 }
