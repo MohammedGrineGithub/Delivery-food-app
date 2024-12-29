ac048860e8f55d1c6a240a53854b6a53ce1905a4
Loordm <lm_grine@esi.dz>
2024-12-30 00:27:34 +0100

    Fix splash screen animation

--- app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = Routes.HOME_PAGE,
+                        startDestination = Routes.SPLASH_SCREEN_PAGE,
                         Modifier.padding(innerPadding)
                     ) {
                         composable(Routes.MAIN_APP) { MainApp(modifier = Modifier.padding(innerPadding), navController = navController) }
