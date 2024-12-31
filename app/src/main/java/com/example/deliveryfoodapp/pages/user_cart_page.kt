ebc3f75728db8939deaa95aae6b42ad39c9b98c2
Loordm <lm_grine@esi.dz>
2024-12-31 21:30:40 +0100

    init user cart page

---
+++ app/src/main/java/com/example/deliveryfoodapp/pages/user_cart_page.kt
@@ -0,0 +1,39 @@
+package com.example.deliveryfoodapp.pages
+
+import androidx.compose.runtime.Composable
+import com.example.deliveryfoodapp.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import com.example.deliveryfoodapp.ui.theme.BlackStroke
+import com.example.deliveryfoodapp.ui.theme.GreyStroke
+import com.example.deliveryfoodapp.ui.theme.Black
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun UserCartPage(navController : NavHostController){
+
+
+    /** Main Page **/
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+            .padding(all = 20.dp)
+    ) {
+
+    }
+}
