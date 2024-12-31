1482ed96ea191f8a79aefc420e2374676012c443
Walid <153912689+Walid380@users.noreply.github.com>
2024-12-31 10:11:40 +0100

    I just started MyOrders Page

--- app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = Routes.PLUS_INFO_PAGE,
+                        startDestination = Routes.MY_ORDERS_PAGE,
                         Modifier.padding(innerPadding)
                     ) {
                         composable(Routes.MAIN_APP) { MainApp(modifier = Modifier.padding(innerPadding), navController = navController) }
--- app/src/main/java/com/example/deliveryfoodapp/pages/my_orders_page.kt
+++ app/src/main/java/com/example/deliveryfoodapp/pages/my_orders_page.kt
@@ -1,10 +1,25 @@
 package com.example.deliveryfoodapp.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun MyOrdersPage(navController : NavHostController) {
-    Text("MyOrdersPage")
+    Column(
+        modifier = Modifier
+            .fillMaxSize() // Fill the entire screen space
+            .padding(16.dp), // Add some padding around the column
+        horizontalAlignment = Alignment.CenterHorizontally, // Center items horizontally
+        //verticalArrangement = Arrangement.SpaceEvenly // Space out items evenly
+    ){
+        Text("MyOrdersPage ...")
+    }
+
 }
