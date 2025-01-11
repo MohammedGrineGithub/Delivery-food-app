0d51f0106deb16c1e2ab51070efdc8da9326410b
MohammedGrineGithub <lm_grine@esi.dz>
2025-01-11 20:27:05 +0100

    Small modification in home page

--- app/src/main/java/com/example/deliveryfoodapp/pages/home_page.kt
+++ app/src/main/java/com/example/deliveryfoodapp/pages/home_page.kt
@@ -63,6 +63,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
 import com.example.deliveryfoodapp.backend_services.restaurant_api.RestaurantEndpoints
 import com.example.deliveryfoodapp.currentRestaurant
+import com.example.deliveryfoodapp.utils.CuisineTypes
+import com.example.deliveryfoodapp.utils.Wilayas
 
 @SuppressLint("NewApi", "MutableCollectionMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -92,82 +94,6 @@ fun HomePage(navController : NavHostController) {
     var cuisine by remember {
         mutableStateOf("Cuisine")
     }
-
-    val wilayas = listOf(
-        Wilaya(0, "None"),
-        Wilaya(1, "Adrar"),
-        Wilaya(2, "Chlef"),
-        Wilaya(3, "Laghouat"),
-        Wilaya(4, "Oum El Bouaghi"),
-        Wilaya(5, "Batna"),
-        Wilaya(6, "Béjaïa"),
-        Wilaya(7, "Biskra"),
-        Wilaya(8, "Béchar"),
-        Wilaya(9, "Blida"),
-        Wilaya(10, "Bouira"),
-        Wilaya(11, "Tamanrasset"),
-        Wilaya(12, "Tébessa"),
-        Wilaya(13, "Tlemcen"),
-        Wilaya(14, "Tiaret"),
-        Wilaya(15, "Tizi Ouzou"),
-        Wilaya(16, "Algiers"),
-        Wilaya(17, "Djelfa"),
-        Wilaya(18, "Jijel"),
-        Wilaya(19, "Sétif"),
-        Wilaya(20, "Saïda"),
-        Wilaya(21, "Skikda"),
-        Wilaya(22, "Sidi Bel Abbès"),
-        Wilaya(23, "Annaba"),
-        Wilaya(24, "Guelma"),
-        Wilaya(25, "Constantine"),
-        Wilaya(26, "Médéa"),
-        Wilaya(27, "Mostaganem"),
-        Wilaya(28, "M’Sila"),
-        Wilaya(29, "Mascara"),
-        Wilaya(30, "Ouargla"),
-        Wilaya(31, "Oran"),
-        Wilaya(32, "El Bayadh"),
-        Wilaya(33, "Illizi"),
-        Wilaya(34, "Bordj Bou Arreridj"),
-        Wilaya(35, "Boumerdès"),
-        Wilaya(36, "El Tarf"),
-        Wilaya(37, "Tindouf"),
-        Wilaya(38, "Tissemsilt"),
-        Wilaya(39, "El Oued"),
-        Wilaya(40, "Khenchela"),
-        Wilaya(41, "Souk Ahras"),
-        Wilaya(42, "Tipaza"),
-        Wilaya(43, "Mila"),
-        Wilaya(44, "Aïn Defla"),
-        Wilaya(45, "Naâma"),
-        Wilaya(46, "Aïn Témouchent"),
-        Wilaya(47, "Ghardaïa"),
-        Wilaya(48, "Relizane"),
-        Wilaya(49, "Timimoun"),
-        Wilaya(50, "Bordj Badji Mokhtar"),
-        Wilaya(51, "Ouled Djellal"),
-        Wilaya(52, "Béni Abbès"),
-        Wilaya(53, "In Salah"),
-        Wilaya(54, "In Guezzam"),
-        Wilaya(55, "Touggourt"),
-        Wilaya(56, "Djanet"),
-        Wilaya(57, "El M'Ghair"),
-        Wilaya(58, "El Meniaa")
-    )
-
-    val cuisineTypes = listOf(
-        CuisineType(0, "None") ,
-        CuisineType(1, "Algerian"),
-        CuisineType(2, "Italian"),
-        CuisineType(3, "Chinese"),
-        CuisineType(4, "French"),
-        CuisineType(5, "Japanese"),
-        CuisineType(6, "Indian"),
-        CuisineType(7, "Mexican"),
-        CuisineType(8, "Thai"),
-        CuisineType(9, "Spanish"),
-        CuisineType(10, "Greek")
-    )
 
     val restaurants = remember {
         mutableStateOf(mutableListOf<Restaurant>())
@@ -259,14 +185,14 @@ fun HomePage(navController : NavHostController) {
                                 .fillMaxWidth()
                                 .clip(RoundedCornerShape(16.dp))
                                 .background(Grey)
-                                .padding(8.dp),
+                                .padding(2.dp),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Icon(
                                 imageVector = Icons.Default.Search,
                                 contentDescription = "Search Icon",
                                 tint = Black,
-                                modifier = Modifier.padding(8.dp)
+                                modifier = Modifier.padding(start = 8.dp, end = 8.dp)
                             )
                             TextField(
                                 value = search,
@@ -317,7 +243,7 @@ fun HomePage(navController : NavHostController) {
                                 ) {
                                 Icon(
                                     painter = painterResource(id = R.drawable.filter),
-                                    contentDescription = "Location",
+                                    contentDescription = "filter",
                                     modifier = Modifier.size(26.dp).background(color = Color.Transparent)
                                         .align(
                                             Alignment.Center
@@ -398,7 +324,7 @@ fun HomePage(navController : NavHostController) {
                                         modifier = Modifier.heightIn(max = 300.dp)
                                             .background(color = Grey)
                                     ) {
-                                        cuisineTypes.forEach { CuisineType ->
+                                        CuisineTypes.ALL_CUISINE_TYPES.forEach { CuisineType ->
                                             DropdownMenuItem(
                                                 text = { Text(text = "${CuisineType.id}- ${CuisineType.name}") },
                                                 onClick = {
@@ -480,7 +406,7 @@ fun HomePage(navController : NavHostController) {
                                         modifier = Modifier.heightIn(max = 300.dp)
                                             .background(color = Grey)
                                     ) {
-                                        wilayas.forEach { Wilaya ->
+                                        Wilayas.ALL_WILAYAS.forEach { Wilaya ->
                                             DropdownMenuItem(
                                                 text = { Text(text = "${Wilaya.id}- ${Wilaya.name}") },
                                                 onClick = {
--- app/src/main/java/com/example/deliveryfoodapp/utils/constants.kt
+++ app/src/main/java/com/example/deliveryfoodapp/utils/constants.kt
@@ -35,8 +35,18 @@ class OrderStatuses {
 class CuisineTypes {
     companion object {
 
-        private val ALL_CUISINE_TYPES = listOf(
-            CuisineType(id = 1,name = "Traditional")
+        val ALL_CUISINE_TYPES = listOf(
+            CuisineType(0, "None") ,
+            CuisineType(1, "Algerian"),
+            CuisineType(2, "Italian"),
+            CuisineType(3, "Chinese"),
+            CuisineType(4, "French"),
+            CuisineType(5, "Japanese"),
+            CuisineType(6, "Indian"),
+            CuisineType(7, "Mexican"),
+            CuisineType(8, "Thai"),
+            CuisineType(9, "Spanish"),
+            CuisineType(10, "Greek")
         )
 
         fun getCuisineTypeByCuisineTypeID(cuisineTypeID : Int) : CuisineType {
@@ -47,8 +57,66 @@ class CuisineTypes {
 class Wilayas {
     companion object {
 
-        private val ALL_WILAYAS = listOf(
-            Wilaya(id = 1, name = "Adrar")
+        val ALL_WILAYAS = listOf(
+            Wilaya(0, "None"),
+            Wilaya(1, "Adrar"),
+            Wilaya(2, "Chlef"),
+            Wilaya(3, "Laghouat"),
+            Wilaya(4, "Oum El Bouaghi"),
+            Wilaya(5, "Batna"),
+            Wilaya(6, "Béjaïa"),
+            Wilaya(7, "Biskra"),
+            Wilaya(8, "Béchar"),
+            Wilaya(9, "Blida"),
+            Wilaya(10, "Bouira"),
+            Wilaya(11, "Tamanrasset"),
+            Wilaya(12, "Tébessa"),
+            Wilaya(13, "Tlemcen"),
+            Wilaya(14, "Tiaret"),
+            Wilaya(15, "Tizi Ouzou"),
+            Wilaya(16, "Algiers"),
+            Wilaya(17, "Djelfa"),
+            Wilaya(18, "Jijel"),
+            Wilaya(19, "Sétif"),
+            Wilaya(20, "Saïda"),
+            Wilaya(21, "Skikda"),
+            Wilaya(22, "Sidi Bel Abbès"),
+            Wilaya(23, "Annaba"),
+            Wilaya(24, "Guelma"),
+            Wilaya(25, "Constantine"),
+            Wilaya(26, "Médéa"),
+            Wilaya(27, "Mostaganem"),
+            Wilaya(28, "M’Sila"),
+            Wilaya(29, "Mascara"),
+            Wilaya(30, "Ouargla"),
+            Wilaya(31, "Oran"),
+            Wilaya(32, "El Bayadh"),
+            Wilaya(33, "Illizi"),
+            Wilaya(34, "Bordj Bou Arreridj"),
+            Wilaya(35, "Boumerdès"),
+            Wilaya(36, "El Tarf"),
+            Wilaya(37, "Tindouf"),
+            Wilaya(38, "Tissemsilt"),
+            Wilaya(39, "El Oued"),
+            Wilaya(40, "Khenchela"),
+            Wilaya(41, "Souk Ahras"),
+            Wilaya(42, "Tipaza"),
+            Wilaya(43, "Mila"),
+            Wilaya(44, "Aïn Defla"),
+            Wilaya(45, "Naâma"),
+            Wilaya(46, "Aïn Témouchent"),
+            Wilaya(47, "Ghardaïa"),
+            Wilaya(48, "Relizane"),
+            Wilaya(49, "Timimoun"),
+            Wilaya(50, "Bordj Badji Mokhtar"),
+            Wilaya(51, "Ouled Djellal"),
+            Wilaya(52, "Béni Abbès"),
+            Wilaya(53, "In Salah"),
+            Wilaya(54, "In Guezzam"),
+            Wilaya(55, "Touggourt"),
+            Wilaya(56, "Djanet"),
+            Wilaya(57, "El M'Ghair"),
+            Wilaya(58, "El Meniaa")
         )
 
         fun getWilayaByWilayaID(wilayaID : Int) : Wilaya {
