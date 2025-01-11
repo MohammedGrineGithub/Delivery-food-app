c8fa7dcb4c3a98fedfa674b7207a99260b999676
Walid <153912689+Walid380@users.noreply.github.com>
2025-01-11 14:52:10 +0100

    I just removed splash animation, DO NOT FORGET TO SYNC GRADLE AGAIN AFTER GIT PULL3

--- app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryfoodapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = Routes.MY_INFORMATION_PAGE,
+                        startDestination = Routes.SPLASH_SCREEN_PAGE,
                         Modifier.padding(innerPadding)
                     ) {
                         composable(Routes.ONBOARDING_PAGE) { OnboardingPage(navController) }
